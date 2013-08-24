d592ed7c6d3de6998536ecc983f192cbacc753ef
Ryan Hughes <Ryan.Hughes@razorfish.com>
2013-08-24 09:33:43 -0400

    continue tutorial

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -11,5 +11,23 @@ Todos.TodoController = Ember.ObjectController.extend({
       model.save();
       return value;
     }
-  }.property('model.isCompleted')
+  }.property('model.isCompleted'),
+
+  isEditing: false,
+
+  editTodo: function () {
+    this.set('isEditing', true);
+  },
+
+  acceptChanges: function () {
+    this.set('isEditing', false);
+    this.get('model').save();
+  },
+  
+  removeTodo: function () {
+    var todo = this.get('model');
+    todo.deleteRecord();
+    todo.save();
+  },
+
 });
