3e0b8aeecabe29ca608bc8881c9a99891946c756
Ryan Hughes <Ryan.Hughes@razorfish.com>
2013-08-26 09:13:27 -0400

    add adapter js

--- js/models/store.js
+++ js/models/store.js
@@ -1,4 +1,14 @@
+// Todos.Store = DS.Store.extend({
+//   revision: 12,
+//   adapter: 'DS.FixtureAdapter'
+// });
+
+
 Todos.Store = DS.Store.extend({
-  revision: 12,
-  adapter: 'DS.FixtureAdapter'
+  revision: 11,
+  adapter: ParseAdapter.create({
+    applicationId: '<YOUR APP ID HERE>',
+    restApiId: '<YOUR REST API KEY HERE>',
+    javascriptId: '<YOUR JAVASCRIPT KEY HERE>'
+  })
 });
