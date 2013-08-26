66ba8022fdc7581e4edf7835c4121a80c35f7b00
Ryan Hughes <Ryan.Hughes@razorfish.com>
2013-08-26 09:06:00 -0400

    adding parse adapter

--- js/models/todo.js
+++ js/models/todo.js
@@ -6,22 +6,47 @@ Todos.Todo = DS.Model.extend({
 Todos.Todo.FIXTURES = [
  {
    id: 1,
-   title: 'Learn Ember.js',
+   title: 'Deposit check',
    isCompleted: true
- },
+ }, 
  {
    id: 2,
-   title: 'Work',
+   title: 'Woodchips',
    isCompleted: true
  },
  {
    id: 3,
-   title: 'Profit!',
+   title: 'Weed wacker',
+   isCompleted: false
+ },
+ {
+   id: 4,
+   title: 'Dog Park',
+   isCompleted: true
+ },
+{
+   id: 5,
+   title: 'Coffeeshop',
    isCompleted: false
  },
 {
-   id: 4,
-   title: 'Super Profit!',
+   id: 6,
+   title: 'Clean',
+   isCompleted: false
+ },
+{
+   id: 7,
+   title: 'Groceries',
+   isCompleted: false
+ },
+ {
+   id: 8,
+   title: 'Grant Park Fest',
+   isCompleted: true
+ },
+  {
+   id: 9,
+   title: 'Perimeter Mall',
    isCompleted: false
  }
 ];
